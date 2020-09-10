b978cc91a90965d56f04e30355fe46f2ace492bd
CLEITON GOMES <90004649@pmenos.com.br>
2020-09-10 06:30:43 -0300

    Adicionar arquivos de projeto.

---
+++ Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                while (true)
+                {
+                    Console.Write("Digite sua Mensagem: ");
+                    string message = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine("Mensagem enviada!");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
